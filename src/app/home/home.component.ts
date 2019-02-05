1fed038949e221e978cc77efa5651fde39ceb9c7
modkhalid <modkhalid5@gmail.com>
2019-02-05 12:09:13 +0530

    adding default animation and styling it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,21 @@
 import { ListService } from './../list.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('animation_name', [
+      transition('void=>*', [
+        style({backgroundColor: 'red', opacity: 0}),
+        animate(2000)
+      ])
+    ])
+
+  ]
 })
 export class HomeComponent implements OnInit {
   constructor(private fb: FormBuilder, private service: ListService) {
