0cf56fbf3b82457c0aeca81c5eadf3e25387875e
modkhalid <modkhalid5@gmail.com>
2019-02-21 15:06:07 +0530

    fade in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
 import { ListService } from './../list.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { fade } from '../animations';
+import { trigger, transition, style , animate} from '@angular/animations';
 
-// import { stat } from 'fs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [fade ]
+  animations: [
+    trigger('fade', [
+      transition('void=>*', [
+        style({
+          backgroundColor: 'red',
+          opacity: 0
+        }),
+        animate(2000)
+      ]),
+      transition('*=>void', [
+        style({
+          backgroundColor: 'yellow',
+          opacity: 0
+        }),
+        animate(2000, style({
+          backgroundColor: 'white',
+          opacity: 1
+        }))
+      ]),
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   constructor(private fb: FormBuilder, private service: ListService) {
