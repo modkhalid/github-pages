82d66f1ecabbfb37846ba594ded0d5e203b9a1c5
modkhalid <modkhalid5@gmail.com>
2019-02-21 17:24:17 +0530

    adding animation which create animation usable by ani transition function as useAnimation(name_var)

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,21 +1,50 @@
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate, state, animation, keyframes, useAnimation } from '@angular/animations';
+
+/*
+ animation fun create animation which can be use by any trigger>transition(':enter',[
+    style({
+      do sometihng here
+    })
+    useAnimation(bounceAnimation)
+  ])
+*/
+export let bounceAnimation = animation(
+  animate('.5s 0.5s ease-out', keyframes([
+    style({
+      offset: .2,
+      opacity: 1,
+      transform: 'translateX(20px)'
+    }),
+    style({
+      offset: 1,
+      opacity: 0,
+      transform: 'translateX(-100%)'
+    }),
+  ]))
+);
 export let fade = trigger('fade', [
-  transition('void=>*', [
+
+  state('void', style({
+    opacity: 0
+  })),
+
+  /*transition('void=>*', [
     style({
       backgroundColor: 'red',
-      opacity: 0
+      // opacity: 0
     }),
     animate(2000)
-  ]),
+  ]),*/
+
+
   transition('*=>void', [
     style({
       backgroundColor: 'yellow',
       opacity: 1
     }),
-    animate(2000, style({
-      backgroundColor: 'white',
-      opacity: 0
-    }))
+    animate(500),
+    useAnimation(bounceAnimation)
   ]),
 ]);
+
 
